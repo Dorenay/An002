4fcbc0e963e9f150fcbda8382174a23ea1cae73e
Dorenay <dorenay@gmail.com>
2018-08-06 18:19:19 +0300

    add hero images

--- src/app/champions.ts
+++ src/app/champions.ts
@@ -1,18 +1,8 @@
 import { Champion } from './champion';
 export const CHAMPIONS : Champion[] = [
     new Champion ('Fiora', 'top'),
-    new Champion ('Graves', 'jungle', 'lorem  isspum'),
+    new Champion ('Gragas', 'jungle', 'lorem  isspum'),
     new Champion ('Shen', 'support', 'lorem  isspum'),
     new Champion ('Ashe', 'ADC', 'lorem  isspum'),
-    new Champion ('Ryze', 'middle', 'lorem  isspum'),
-    new Champion ('Fiora', 'top'),
-    new Champion ('Graves', 'jungle', 'lorem  isspum'),
-    new Champion ('Shen', 'support', 'lorem  isspum'),
-    new Champion ('Ashe', 'ADC', 'lorem  isspum'),
-    new Champion ('Ryze', 'middle', 'lorem  isspum'),
-    new Champion ('Fiora', 'top'),
-    new Champion ('Graves', 'jungle', 'lorem  isspum'),
-    new Champion ('Shen', 'support', 'lorem  isspum'),
-    new Champion ('Ashe', 'ADC', 'lorem  isspum'),
-    new Champion ('Ryze', 'middle', 'lorem  isspum')      
+    new Champion ('Arhi', 'middle', 'lorem  isspum')
 ];
